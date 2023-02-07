d18b5540222a50c1b936cd9df5f626a28edb2f83
JunTK <kjt3015@naver.com>
2023-02-07 09:14:22 +0000

    jun

--- OnlineChat/index.js
+++ OnlineChat/index.js
@@ -44,6 +44,18 @@ app.get('/', function(request, response){
     })
 })
 
+io.sockets.on('connection', function(socket){
+    console.log('유저 접속 됨')
+    
+    socket.on('send', function(data){
+        console.log('전달된 메시지:', data.msg)
+    })
+    
+    socket.on('disconnect', function(){
+        console.log('접속 종료')
+    })
+})
+
 // 서버를 81포트로 listen
 server.listen(81, function(){
     console.log('서버 실행중..')
